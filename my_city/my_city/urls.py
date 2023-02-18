05a4fb3c615fa320a62c17a209f6f44b389d1409
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2023-02-18 15:20:05 -0800

    setting_static

--- my_city/my_city/urls.py
+++ my_city/my_city/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
-from .settings import DEBUG
+
+from .settings import DEBUG, STATIC_ROOT, STATIC_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(STATIC_URL, document_root=STATIC_ROOT)
+
 
 if DEBUG:
     import debug_toolbar
