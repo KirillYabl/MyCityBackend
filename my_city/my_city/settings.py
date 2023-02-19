aef5e0ba0ae340c3d3c1d3e7a9563642cbe9ca40
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2023-02-18 16:18:53 -0800

    setting_static

--- my_city/my_city/settings.py
+++ my_city/my_city/settings.py
@@ -108,8 +108,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = (BASE_DIR / 'static')
-MEDIA_ROOT = (BASE_DIR / 'media')
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'media'
 STATICFILES_DIRS = ((BASE_DIR / 'static'),)
 
 
