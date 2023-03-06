f20623afbbea4e43170f40ee49037da3041a9ee6
KirillYabl <kirill-yablunovskii@mail.ru>
2023-03-06 19:29:18 +0300

    Add models with semi static information
    Semi static means that will be changing rarely

--- my_city/quest_app/admin.py
+++ my_city/quest_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models as m
+
+
+@admin.register(m.ContactType)
+class ContactTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(m.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(m.FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    pass
--- my_city/quest_app/models.py
+++ my_city/quest_app/models.py
@@ -1,3 +1,50 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.template.defaultfilters import truncatechars
 
-# Create your models here.
+
+class ContactType(models.Model):
+    """Тип контакта нужен, чтобы фронтенд понимал, в какой блок пойдет контакт"""
+    name = models.CharField('наименование типа контакта', max_length=64)
+
+    class Meta:
+        verbose_name = 'тип контакта'
+        verbose_name_plural = 'типы контактов'
+
+    def __str__(self):
+        return self.name
+
+
+class Contact(models.Model):
+    """Контакты: соцсети, телефоны, ссылка на политики и т.д. для футера"""
+    contact_type = models.ForeignKey(
+        verbose_name='тип контакта',
+        to=ContactType,
+        on_delete=models.PROTECT,
+        related_name='types',
+    )
+    contact = models.TextField('контакт', unique=True)
+    description = models.TextField('описание', blank=True)
+    order = models.PositiveSmallIntegerField('порядок показа', unique=True, validators=[MinValueValidator(1)])
+
+    class Meta:
+        verbose_name = 'контакт'
+        verbose_name_plural = 'контакты'
+
+    def __str__(self):
+        return self.contact
+
+
+class FAQ(models.Model):
+    """Часто задаваемые вопросы с ответами"""
+    question = models.TextField('вопрос', unique=True)
+    answer = models.TextField('ответ')
+    order = models.PositiveSmallIntegerField('порядок показа', unique=True, validators=[MinValueValidator(1)])
+
+    class Meta:
+        verbose_name = 'часто задаваемый вопрос'
+        verbose_name_plural = 'часто задаваемые вопросы'
+
+    def __str__(self):
+        truncate_length = 97  # 100 - 3 (symbols for ellipsis)
+        return truncatechars(self.question, truncate_length)
