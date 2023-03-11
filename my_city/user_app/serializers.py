87323865de43a5e988037575e0b8c8731c45446e
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2023-03-11 00:41:48 -0800

    create_jwt_users_v2

--- my_city/user_app/serializers.py
+++ my_city/user_app/serializers.py
@@ -26,8 +26,3 @@ class LoginUserSerializer(serializers.Serializer):
     email = serializers.CharField()
     password = serializers.CharField()
 
-    def validate(self, data):
-        user = authenticate(**data)
-        if user and user.is_active:
-            return user
-        raise serializers.ValidationError("Invalid Details.")
--- my_city/user_app/views.py
+++ my_city/user_app/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.hashers import check_password
 from knox.models import AuthToken
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 
 from my_city.settings import TOKEN_INDEX
 
+from .models import User
 from .serializers import (CreateUserSerializer, LoginUserSerializer,
                           UserSerializer)
 
@@ -27,12 +29,13 @@ class LoginAPI(generics.GenericAPIView):
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        return Response({
-            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+        user = User.objects.get(email=serializer.data['email'])
+        if check_password(request.POST['password'], user.password):
+            return Response({
             "token": AuthToken.objects.create(user)[TOKEN_INDEX]
-        })
-
+            })
+        else:
+            return Response('Неверный пароль!')
 
 class UserAPI(generics.RetrieveAPIView):
     permission_classes = [permissions.IsAuthenticated, ]
